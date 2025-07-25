2b800b47667d5612e138631d2e60a345fc41daff
Gabriel Sousa <ghostamino08@gmail.com>
2025-07-25 07:40:50 -0300

    model layer

---
+++ src/main/java/com/GabSkyDev/to_do_list/model/Priority.java
@@ -0,0 +1,7 @@
+package com.GabSkyDev.to_do_list.model;
+
+public enum Priority {
+    HIGH,
+    MEDIUM,
+    LOW
+}
---
+++ src/main/java/com/GabSkyDev/to_do_list/model/Task.java
@@ -0,0 +1,65 @@
+package com.GabSkyDev.to_do_list.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private Boolean state;
+    private Priority priority;
+
+    public Task(){}
+
+    public Task(Long id, String name, String description, Boolean state, Priority priority) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.state = state;
+        this.priority = priority;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getState() {
+        return state;
+    }
+
+    public void setState(Boolean state) {
+        this.state = state;
+    }
+
+    public Priority getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Priority priority) {
+        this.priority = priority;
+    }
+}
