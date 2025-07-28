f6776a2817bb922d236afc6674ca053d02a78ed0
Gabriel Sousa <ghostamino08@gmail.com>
2025-07-28 06:40:52 -0300

    Initialize React app and split backend/frontend structure

--- backend/src/main/java/com/GabSkyDev/to_do_list/controller/TaskController.java
+++ backend/src/main/java/com/GabSkyDev/to_do_list/controller/TaskController.java
@@ -8,9 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/task")
 public class TaskController {
